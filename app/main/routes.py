7017e51fddc6ae00b1e35611abd86e486bb6d22d
xyh302a <490453102@qq.com>
2018-06-11 12:47:08 +0800

    revise

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g, current_app
+from flask import render_template, flash, redirect, url_for, request, g, \
+	jsonify, current_app
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
+from guess_language import guess_language
 from app import db
 from app.main.forms import EditProfileForm, PostForm, SearchForm, MessageForm
 from app.models import User, Post, Message, Notification
@@ -170,9 +172,9 @@ def messages():
     messages = current_user.messages_received.order_by(
         Message.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.message', page=messages.next_num) \
+    next_url = url_for('main.messages', page=messages.next_num) \
         if messages.has_next else None
-    prev_url = url_for('main.message', page=messages.prev_num) \
+    prev_url = url_for('main.messages', page=messages.prev_num) \
         if messages.has_prev else None
     return render_template('messages.html', messages=messages.items,
                            next_url=next_url, prev_url=prev_url)
